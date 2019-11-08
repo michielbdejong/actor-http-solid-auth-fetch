86b279e90698378c64dd20196ae723a0dc89ad3b
Michiel de Jong <michiel@unhosted.org>
2019-11-08 14:06:30 +0100

    fetch mock should reject, not throw

--- test/__mocks__/isomorphic-fetch.ts
+++ test/__mocks__/isomorphic-fetch.ts
@@ -1,6 +1,7 @@
 (<any> global).fetch = (input: any, init: any) => {
   if (input.url === 'https://www.google.comz/') {
-    throw new Error('FetchError: request to https://google.comz/ failed, reason: getaddrinfo ENOTFOUND google.comz');
+    const message = 'FetchError: request to https://google.comz/ failed, reason: getaddrinfo ENOTFOUND google.comz';
+    return Promise.reject(new Error(message));
   }
   return Promise.resolve({ status: input.url === 'https://www.google.com/' ? 200 : 404 });
 };
